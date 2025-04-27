ffdca66d4d3b31291b8330cf8730a4db6db22956
Manu <manueljesusrejonsantana@gmail.com>
2025-04-26 20:02:01 -0600

    chore: create website

---
+++ src/lib/wp.ts
@@ -0,0 +1,55 @@
+const domain = import.meta.env.WP_DOMAIN
+const apiUrl = `${domain}/wp-json/wp/v2`
+
+export const getPageInfo = async (slug: string) => {
+    const response = await fetch(`${apiUrl}/pages?=${slug}`)
+    if (!response.ok) throw new Error("Error en el fetching de data")
+
+    const [data] = await response.json()
+    const { title: { rendered: title }, content: { rendered: content } } = data
+
+    return { title, content }
+}
+
+export const getAllPostsSlugs = async () => {
+    const response = await fetch(`${apiUrl}/posts?per_page=100`)
+    if (!response.ok) throw new Error("Error en el fetching de data");
+
+    const results = await response.json()
+    if (!results.length) throw new Error("No se encontraron posts")
+
+    const slugs = results.map((post) => post.slug)
+    return slugs
+}
+
+export const getPostInfo = async (slug: string) => {
+    const response = await fetch(`${apiUrl}/posts?slug=${slug}`)
+    if (!response.ok) throw new Error("Error en el fetching de data")
+
+    const [data] = await response.json()
+    const { title: { rendered: title }, content: { rendered: content }, yoast_head_json: seo } = data
+
+    return { title, content, seo }
+}
+
+export const getLatestPosts = async ({ perPage = 10 }: { perPage?: number } = {}) => {
+    const response = await fetch(`${apiUrl}/posts?per_page=${perPage}&_embed`)
+    if (!response.ok) throw new Error("Error en el fetching de data")
+
+    const results = await response.json()
+    if (!results.length) throw new Error("No hay posts para mostrar");
+
+    const posts = results.map(post => {
+        const title = post.title.rendered
+        const excerpt = post.excerpt.rendered
+        const content = post.content.rendered
+        const { date, slug } = post
+
+        const feturedImage = post._embedded['wp:featuredmedia'][0].source_url
+
+        return { title, excerpt, content, date, slug, feturedImage }
+    })
+
+    return posts
+}
+
